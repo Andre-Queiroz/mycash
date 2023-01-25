ae42bdceaa950cd62a134a49da15d67ae82f6b5a
Andre-Queiroz <andretorresdequeiroz@gmail.com>
2023-01-25 00:40:49 -0300

    Initial commit

---
+++ src/main/java/queiroz/mycash/MyCashApplication.java
@@ -0,0 +1,13 @@
+package queiroz.mycash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCashApplication.class, args);
+	}
+
+}
